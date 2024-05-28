2b499ce117e5075f30740ba9567a7a3db288627b
Noah McMahon <nam1414@uw.edu>
2024-05-28 00:43:46 -0700

    Added JS and CSS

---
+++ main.js
@@ -0,0 +1,19 @@
+var svg = d3.select('svg');
+
+// Get layout parameters
+var svgWidth = +svg.attr('width');
+var svgHeight = +svg.attr('height');
+
+var padding = {t: 60, r: 40, b: 30, l: 40};
+
+// Compute chart dimensions
+var chartWidth = svgWidth - padding.l - padding.r;
+var chartHeight = svgHeight - padding.t - padding.b;
+
+// Compute the spacing for bar bands based on all 26 letters
+var barBand = chartHeight / 26;
+var barHeight = barBand * 0.7;
+
+// Create a group element for appending chart elements
+var chartG = svg.append('g')
+    .attr('transform', 'translate('+[padding.l, padding.t]+')');
