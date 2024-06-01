cd426ae608012e882834d9b081b66ccd41ce6e95
Noah McMahon <nam1414@uw.edu>
2024-06-01 00:14:20 -0700

    Completed Visualization

--- main.js
+++ main.js
@@ -1,114 +1,175 @@
 // Global function called when select element is changed
 function onCategoryChanged() {
-    var select = d3.select('#categorySelect').node();
-    var category = select.options[select.selectedIndex].value;
-    var word = category;
-    word = word.toUpperCase();
-    var file = word + '.csv';
-    console.log(file);
+    index = document.getElementById("categorySelect").selectedIndex;
+    return index;
 }
 
-var chartDiv = document.getElementById('chart');
+var svg = d3.select('svg');
+var svgWidth = +svg.attr('width');
+var svgHeight = +svg.attr('height');
+var padding = {t: 15, r: 5, b: 45, l: 55};
+chartWidth = svgWidth - padding.r - padding.l;
+chartHeight = svgHeight - padding.t - padding.b;
 
-d3.csv('CLT.csv').then(function(dataset) {
-    weather = dataset;
+var parseDate = d3.utcFormat("%Y-%m-%d");
+var dateDomain = [new Date(2014, 7, 1), new Date(2015, 6, 30)];
+var tempDomain = [-20, 120];
 
-    var weather_actual_mean_temp = [];
-    for (let i = 0; i < weather.length; i++)
-    {
-      weather_actual_mean_temp[i] = weather[i].actual_mean_temp;
+var xScale = d3.scaleTime()
+    .domain(dateDomain)
+    .range([0, chartWidth])
+
+var yScale = d3.scaleLinear()
+    .domain(tempDomain)
+    .range([chartHeight, 0])
+
+var clip = svg.append("defs").append("svg:clipPath")
+    .attr("id", "clip")
+    .append("svg:rect")
+    .attr("width", svgWidth )
+    .attr("height", svgHeight )
+    .attr("x", 0)
+    .attr("y", 0);
+
+var brush = d3.brushX()
+    .extent( [ [0,0], [svgWidth, svgHeight] ] )
+    .on("end", zoomChart)
+
+var f = [];
+
+Promise.all([
+  d3.csv('CLT.csv'),
+  d3.csv('CQT.csv'),
+  d3.csv('IND.csv'),
+  d3.csv('JAX.csv'),
+  d3.csv('MDW.csv'),
+  d3.csv('PHL.csv'),
+  d3.csv('PHX.csv')
+]).then(function(files) {
+  id = onCategoryChanged();
+
+  weather = files[id];
+
+  f = files;
+
+  document.getElementById('categorySelect').addEventListener('input', function() {
+    onCategoryChanged()
+    updateChart(files[onCategoryChanged()]);
+  })
+
+  updateChart(weather);
+});
+
+function updateChart(weather) {
+  d3.selectAll("svg > *").remove();
+
+  console.log(xScale.domain());
+
+  const months = {0 : 'Jan', 1 : 'Feb', 2 : 'Mar', 3 : 'Apr', 4 : 'May', 5 : 'Jun', 6 : 'Jul', 7 : 'Aug', 8 : 'Sep', 9 : 'Oct', 10 : 'Nov', 11 : 'Dec'}
+
+	function dateFormat(dateStr) {
+    var parts = String(dateStr).split('-')
+    return new Date(Number(parts[0]), Number(parts[1]), Number(parts[2]))
+  }
+	for (var i = 0; i < weather.length; i++) {
+		weather[i]['Date'] = dateFormat(weather[i]['date'])
+	}
+
+  weather = weather.filter(function(d) {
+    return (d.Date < xScale.domain()[1] && d.Date > xScale.domain()[0]);
+  });
+
+  var dates = _.map(weather, 'Date');
+
+  var xBand = d3.scaleBand().domain(d3.range(-1, dates.length)).range([0, chartWidth]).padding(0.3);
+
+  var chart = svg.append('g')
+    .attr('transform', 'translate('+[padding.l, padding.t]+')');
+
+  var xScale2 = d3.scaleLinear()
+    .domain([-1, dates.length])
+    .range([0, chartWidth])
+
+  svg.append("g")
+    .attr("clip-path", "url(#clip)")
+    .append('g')
+    .attr("class", "brush")
+    .call(brush);
+
+  var candles = chart.selectAll('.candle')
+    .data(weather)
+    .enter()
+    .append('rect')
+    .attr('class', 'candle')
+    .attr('x', (d, i) => xScale2(i) - xBand.bandwidth())
+    .attr('y', d => yScale(Math.max(d.actual_min_temp, d.actual_max_temp)))
+    .attr('width', xBand.bandwidth())
+    .attr('height', d => (d.actual_min_temp === d.actual_max_temp) ? 1 : yScale(Math.min(d.actual_min_temp, d.actual_max_temp))-yScale(Math.max(d.actual_min_temp, d.actual_max_temp)))
+    .attr("fill", d => (d.actual_min_temp === d.actual_max_temp) ? "yellow" : (d.actual_min_temp > d.actual_max_temp) ? "blue" : "red");
+
+  var stems = chart.selectAll("g.line")
+    .data(weather)
+    .enter()
+    .append("line")
+    .attr("class", "stem")
+    .attr("x1", (d, i) => xScale2(i) - xBand.bandwidth()/2)
+    .attr("x2", (d, i) => xScale2(i) - xBand.bandwidth()/2)
+    .attr("y1", d => yScale(d.average_max_temp))
+    .attr("y2", d => yScale(d.average_min_temp))
+    .attr("stroke", d => (d.actual_min_temp === d.actual_max_temp) ? "white" : (d.actual_min_temp > d.actual_max_temp) ? "blue" : "red");
+
+  var line = chart.append("path")
+    .datum(weather)
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("stroke-width", 3)
+    .attr("d", d3.line()
+      .x(function(d, i) { return (xScale2(i) - xBand.bandwidth()) })
+      .y(function(d) { return (yScale(d.actual_mean_temp)) })
+    )
+
+  var xAxis = chart.append('g')
+    .call(d3.axisBottom(xScale))
+    .attr("transform", "translate(0,"+chartHeight+")")
+    .attr('class', 'x axis');
+
+  chart.append("g")
+    .call(d3.axisLeft(yScale))
+    .attr("class", "y axis");
+
+  chart.append("text")
+    .attr("class", "x axis-label")
+    .text("Date")
+    .attr("transform", "translate("+[(chartWidth/2),(chartHeight + 34)]+")")
+
+  chart.append("text")
+    .attr("class", "y axis-label")
+    .text("Temperature (\u00B0C)")
+    .attr("transform", "translate("+[-30,(chartHeight/2) + 80]+")rotate(-90)")
+}
+
+var idleTimeout
+function idled() { idleTimeout = null; }
+
+function zoomChart() {
+  extent = d3.event.selection
+
+  // If no selection, back to initial coordinate. Otherwise, update X axis domain
+  if(!extent){
+    if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
+      xScale.domain(dateDomain);
+    }else{
+      xScale.domain([ xScale.invert(extent[0]), xScale.invert(extent[1]) ])
+      svg.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
     }
 
-    var weather_dates = [];
-    for (let i = 0; i < weather.length; i++)
-    {
-      weather_dates[i] = weather[i].date;
-    }
+  // Update axis and line position
+  //xAxis.transition().duration(1000).call(d3.axisBottom(xScale))
+  updateChart(f[onCategoryChanged()]);
+}
 
-    var weather_actual_max_temp = [];
-    for (let i = 0; i < weather.length; i++)
-    {
-      weather_actual_max_temp[i] = weather[i].actual_max_temp;
-    }
-
-    var weather_actual_min_temp = [];
-    for (let i = 0; i < weather.length; i++)
-    {
-      weather_actual_min_temp[i] = weather[i].actual_min_temp;
-    }
-
-    var weather_average_max_temp = [];
-    for (let i = 0; i < weather.length; i++)
-    {
-      weather_average_max_temp[i] = weather[i].average_max_temp;
-    }
-
-    var weather_average_min_temp = [];
-    for (let i = 0; i < weather.length; i++)
-    {
-      weather_average_min_temp[i] = weather[i].average_min_temp;
-    }
-    
-    var trace1 = {
-  
-        x: weather_dates, 
-        
-        close: weather_actual_max_temp, 
-        
-        decreasing: {line: {color: '#7F7F7F'}},
-        
-        high: weather_average_max_temp, 
-        
-        increasing: {line: {color: '#17BECF'}},
-        
-        line: {color: 'rgba(31,119,180,1)'}, 
-        
-        low: weather_average_min_temp, 
-        
-        open: weather_actual_min_temp, 
-        
-        type: 'candlestick', 
-        xaxis: 'x', 
-        yaxis: 'y'
-      };
-
-      var trace2 = {
-        x: weather_dates,
-        y: weather_actual_mean_temp,
-        line: {
-          color: 'rgb(0, 0, 0)',
-          width: 2
-        },
-        type: 'scatter'
-      }
-      
-      var data = [trace1, trace2];
-      
-      var layout = {
-        dragmode: 'zoom', 
-        margin: {
-          r: 10, 
-          t: 25, 
-          b: 40, 
-          l: 60
-        }, 
-        showlegend: false,
-        xaxis: {
-          autorange: true, 
-          domain: [0, 1], 
-          range: ['2014-7-1', '2015-6-30'], 
-          rangeslider: {range: ['2014-7-1', '2015-6-30']}, 
-          title: 'Date', 
-          type: 'date'
-        }, 
-        yaxis: {
-          autorange: true, 
-          domain: [0, 1], 
-          range: [-20, 120],
-          title: 'Temperature (&deg;C)',
-          type: 'linear'
-        }
-    };
-      
-    Plotly.newPlot(chartDiv, data, layout);
-})
+// If user double click, reinitialize the chart
+svg.on("dblclick",function(){
+  xScale.domain(dateDomain);
+  updateChart(f[onCategoryChanged()]);
+});
